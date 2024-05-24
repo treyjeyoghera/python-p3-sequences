9756e8b6ce9b2acf5103baa7ca50d05b64bc90cd
treyjeyoghera <treyjeyogheraeugene@gmail.com>
2024-05-24 15:01:14 +0300

    wsd

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
+# With range:
+
 def print_fibonacci(length):
-    pass
+
+	new= []
+	if length > 0:
+		new.append(0)
+	if length >= 2:
+		new.append(1)
+		for i in range(2, length):
+			new.append(new[-1] + new[-2])
+
+	print(new)
